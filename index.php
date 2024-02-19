1402ca1c72969d0cd46067f7f0213983f6621a69
wapeety <wapeety@protonmail.com>
2024-02-19 13:40:32 +0100

    minor fixes

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-include_once 'controller/controller.php';
+include_once 'controllers/controller.php';
 
 check_login();
 ?>
--- model/DAO/adminDAO.php
+++ model/DAO/adminDAO.php
@@ -6,6 +6,16 @@ class AdminDAO {
     public function __construct(){
         $this->connection = new Connection();
     }
+
+    #maybe i'll remove this function, it's not necessary
+    public function createAdmin($admin){
+        $sql = "INSERT INTO admin (name, password) VALUES (?, ?)";
+        $stmt = $this->connection->prepare($sql);
+        $admin->setPassword($admin->getPassword(), true); #encrypt password
+        $stmt->bind_param('ss', $admin->getName(), $admin->getPassword());
+        $stmt->execute();
+        $stmt->close();
+    }
     
     public function getAdmin($username, $password){
         $sql = "SELECT * FROM admin WHERE name = ? LIMIT 1";
